3cc3d54b0eb77ad656672ec4b78dba76ac9ce5a5
Prasidh Aggarwal <aprasidh@gmail.com>
2023-10-21 14:47:46 -0700

    added legend and hover efffect

--- js/prasidh.js
+++ js/prasidh.js
@@ -55,13 +55,13 @@ function create_grouped_bar_chart() {
   console.log(groupedData);
   let svg = d3.select("#grouped-bar-chart");
 
-  let margin = { top: 20, right: 40, bottom: 50, left: 60 };
+  let margin = { top: 60, right: 40, bottom: 50, left: 60 };
   let width = +svg.attr("width") - margin.left - margin.right;
   let height = +svg.attr("height") - margin.top - margin.bottom;
 
   let g = svg
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   // Define the x and y scales
   let x0 = d3.scaleBand().rangeRound([0, width]).paddingInner(0.1);
@@ -73,7 +73,7 @@ function create_grouped_bar_chart() {
   // Define the color scale
   let z = d3.scaleOrdinal(d3.schemeTableau10);
 
-  let keys = ["Work", "Home", "Pub", "Restaurant", "Unknown"];
+  let keys = ["Work", "Pub", "Restaurant"];
   x0.domain(groupedData.map((d) => d.key));
   x1.domain(keys).rangeRound([0, x0.bandwidth()]);
   y.domain([
@@ -82,13 +82,25 @@ function create_grouped_bar_chart() {
   ]).nice();
 
   // Draw the bars
-  g
-    .append("g")
+  g.append("g")
     .selectAll("bars")
     .data(groupedData)
     .enter()
     .append("g")
+    .attr("class", "bar-group")
     .attr("transform", (d) => "translate(" + x0(d.key) + ",0)")
+    .on("mouseover", function () {
+      // Decrease the opacity of other bars on hover
+      d3.selectAll(".bar-group")
+        .transition()
+        .duration(200)
+        .style("opacity", 0.6);
+      d3.select(this).transition().duration(200).style("opacity", 1);
+    })
+    .on("mouseout", function () {
+      // Restore the default opacity of all bars on mouseout
+      d3.selectAll(".bar-group").transition().duration(200).style("opacity", 1);
+    })
     .selectAll("rect")
     .data((d) => keys.map((key) => ({ key: key, value: d[key] }))) // Create a new array of objects with properties 'key' and 'value'
     .enter()
@@ -100,13 +112,43 @@ function create_grouped_bar_chart() {
     .attr("fill", (d) => z(d.key));
 
   // Add the x-axis
-  g
-    .append("g")
+  g.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x0));
 
   // Add the y-axis
   g.append("g").call(d3.axisLeft(y));
+
+  // Create a legend
+  const legend = g
+    .append("g")
+    .attr("class", "legend")
+    .attr("transform", "translate(" + (width - 80) + ", -40)"); // Adjust the translation to position the legend
+
+  const legendDotSize = 8; // Size of the colored circles in the legend
+  const legendSpacing = 10; // Spacing between legend items
+
+  const legendKeys = legend
+    .selectAll(".legend-key")
+    .data(keys)
+    .enter()
+    .append("g")
+    .attr("class", "legend-key")
+    .attr(
+      "transform",
+      (d, i) => "translate(0, " + i * 1.5 * legendSpacing + ")"
+    ); // Adjust spacing between legend items
+
+  legendKeys
+    .append("circle")
+    .attr("r", legendDotSize / 2) // Radius of the colored circles
+    .attr("fill", (d) => z(d)); // Use the same color scale for legend
+
+  legendKeys
+    .append("text")
+    .attr("x", legendDotSize + 5) // Adjust the spacing between the circle and the text
+    .attr("y", legendDotSize / 2) // Center the text vertically in the circle
+    .text((d) => d); // Display the corresponding key next to the colored circle
 
   // Add the x-axis label
   svg
@@ -116,21 +158,22 @@ function create_grouped_bar_chart() {
       "translate(" +
         (width / 2 + margin.left) +
         " ," +
-        (height + margin.top + margin.bottom - 20) +
+        (height + margin.top + margin.bottom - 10) +
         ")"
     )
     .style("text-anchor", "middle")
-    .style("font-size","10px")
+    .style("font-size", "14px")
     .text("Portion of Day");
 
-    svg.append("text")
+  svg
+    .append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", margin.left / 2 - 30) // Adjust this value as needed
-    .attr("x", 0 - (height / 2))
+    .attr("x", 0 - (height + margin.top) / 2)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
-    .style("font-size","10px")
-    .text("Total Commutes");
+    .style("font-size", "14px")
+    .text("Number of Commutes");
 }
 
 function get_month_and_day() {
